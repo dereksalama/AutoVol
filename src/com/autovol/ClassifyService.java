25bcfb054167714eae3c0f779ceeb6b0d88e38ac
Derek Salama <dereksalama@gmail.com>
2014-04-08 17:03:55 -0400

    ditto

---
+++ src/com/autovol/ClassifyService.java
@@ -0,0 +1,77 @@
+package com.autovol;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+
+public class ClassifyService extends IntentService {
+	
+	public ClassifyService() {
+		super("ClassifyService");
+	}
+
+	public static final String FILE_NAME = "classify_result_file";
+	
+	private void saveResult(String result) {
+		try {
+			OutputStream output = new BufferedOutputStream(openFileOutput(FILE_NAME, 
+					Context.MODE_APPEND));
+			output.write(result.getBytes());
+			output.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		if (!CurrentStateListener.get().dataIsReady()) {
+			return;
+		}
+		
+    	String reqUrl = AppPrefs.getBaseUrl(this) + MainActivity.GM_URL + "?" + 
+    			"target=" + CurrentStateListener.get().currentStateJson();
+		String time = new SimpleDateFormat("dd HH:mm", Locale.US).format(new Date());
+    	
+    	HttpURLConnection urlConnection = null;
+		try {
+			URL url = new URL(reqUrl);
+			urlConnection = (HttpURLConnection) url.openConnection();
+			InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+			BufferedReader streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8")); 
+			StringBuilder responseStrBuilder = new StringBuilder();
+
+			String inputStr;
+			while ((inputStr = streamReader.readLine()) != null)
+				responseStrBuilder.append(inputStr);
+
+			saveResult(time + ": " + responseStrBuilder.toString());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (urlConnection != null)
+				urlConnection.disconnect();
+		}
+
+	}
+
+}
---
+++ src/com/autovol/ResultsActivity.java
@@ -0,0 +1,81 @@
+package com.autovol;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+public class ResultsActivity extends ListActivity {
+	
+	ArrayAdapter<String> adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_results);
+		
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		setListAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.results, menu);
+		return true;
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		loadData();
+	}
+	
+	private void loadData() {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(
+					new FileReader(getFileStreamPath(ClassifyService.FILE_NAME)));
+			
+			adapter.clear();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				adapter.add(line);
+			}
+			adapter.notifyDataSetChanged();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.results_action_delete) {
+			File f = getFileStreamPath(ClassifyService.FILE_NAME);
+			if (f.exists()) {
+				f.delete();
+				adapter.clear();
+			}
+			return true;
+		} else if (id == R.id.results_action_refresh) {
+			loadData();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
